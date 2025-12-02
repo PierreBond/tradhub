3dcbc402e899e6b00823210e20541dbbb1226305
PierreBond <tinaantwi1957@gmail.com>
2025-12-02 16:05:25 +0100

    update 1

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -3,6 +3,7 @@
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { mockUser } from '@/lib/data';
+// import "@/styles/global.css"
 
 export default function Navbar() {
   const pathname = usePathname();
@@ -10,11 +11,11 @@ export default function Navbar() {
   const isActive = (path: string) => pathname === path;
   
   return (
-    <nav className="bg-background border-b border-border">
-      <div className="container mx-auto flex h-16 items-center justify-between px-4">
+    <nav className=" nav-blur fixed top-0 left-0 right-0 z-50 px-6 py-4">
+      <div className=" max-w-7x1 mx-auto flex items-center justify-between">
         <div className="flex items-center space-x-8">
-          <h1 className="text-xl font-bold text-primary">TradeHub</h1>
-          <div className="flex items-center space-x-6">
+          <h1 className="text-x2 font-bold text-primary">TradeHub</h1>
+          <div className="hidden md:flex items-center space-x-6">
             <Link
               href="/"
               className={`transition-colors ${
@@ -48,7 +49,7 @@ export default function Navbar() {
           </div>
         </div>
         <div className="flex items-center space-x-4">
-          <div className="text-secondary">
+          <div className="text-secondary bg-white px-4  py-2 rounded-lg shadow-sm ">
             Balance:{' '}
             <span className="font-semibold text-primary">
               ${mockUser.balance.toLocaleString()}
